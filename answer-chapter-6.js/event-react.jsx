f6b8315c6e3a95017fde25f6fc8fca783eeca2eb
winatama <winatamafza@gmail.com>
2024-03-27 20:44:26 +0700

    first commit

---
+++ answer-chapter-6.js/event-react.jsx
@@ -0,0 +1,7 @@
+const shoot = () => {
+    alert ("GOAAALL");
+}
+
+return (
+    <button onClick={shoot}>Take the Shot!</button>
+);
---
+++ answer-chapter-6.js/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactDOM } from "react";
+import App from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App></App>
+    </React.StrictMode>
+    document.getElementById('root')
+);
